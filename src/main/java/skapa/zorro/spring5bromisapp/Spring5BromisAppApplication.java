af0ec4ce17a148fce470a0d086acab27ffbab1cc
Alberto Gonzalez <alb.gon.vil@gmail.com>
2022-09-22 09:49:17 -0500

    comit inicial del proyecto

---
+++ src/main/java/skapa/zorro/spring5bromisapp/Spring5BromisAppApplication.java
@@ -0,0 +1,13 @@
+package skapa.zorro.spring5bromisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5BromisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5BromisAppApplication.class, args);
+	}
+
+}
